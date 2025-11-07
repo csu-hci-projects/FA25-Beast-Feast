e923d56e2ed112cbb1d081b7ec53df0e42a770b9
EthanAmundson0 <eamunds3@colostate.edu>
2025-11-06 20:26:46 -0700

    Creature Size Feature
    -Added Script which gives stats to creatures for size
    -Added Action for player to consume creatures smaller than itself
    -Added UI element to display players size

--- Assets/Scripts/DirectedAgent.cs
+++ Assets/Scripts/DirectedAgent.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
 using UnityEngine.AI;
+using System.Collections.Generic;
+using TMPro;
 
 public class DirectedAgent : MonoBehaviour
 {
     NavMeshAgent agent;
     Animator animator;
+    [SerializeField] TextMeshProUGUI sizeText;
+    [SerializeField] float attackRadius = 3f;
+
+    private SizeStats playerStats;
 
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
         agent.updateRotation = false; // We’ll handle rotation manually
+        playerStats = GetComponent<SizeStats>();
+    }
+
+    void Start()
+    {
+        SetSizeText();
     }
 
     void Update()
@@ -36,8 +48,49 @@ public class DirectedAgent : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(inputDir), 0.15f);
         }
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ProcessAttack();
+            SetSizeText();
+        }
+
         // Animation blend (same parameter as before)
         float speed = inputDir.magnitude * agent.speed;
         animator.SetFloat("forwardSpeed", speed);
+
+    }
+
+    private void ProcessAttack()
+    {
+        GameObject[] enemies = GetEnemiesInAttackRange();
+            foreach (GameObject enemy in enemies)
+            {
+                bool eatEnemy = playerStats.TryEatEnemy(enemy.GetComponent<SizeStats>().GetSize());
+                if (eatEnemy)
+                {
+                    Destroy(enemy);
+                }
+            }
+    }
+
+    private GameObject[] GetEnemiesInAttackRange()
+    {
+        GameObject[] taggedObjects = GameObject.FindGameObjectsWithTag("Enemy");
+        List<GameObject> closeEnemies = new List<GameObject>();
+        foreach (GameObject obj in taggedObjects)
+        {
+            float distance = Vector3.Distance(transform.position, obj.transform.position);
+
+            if (distance <= attackRadius)
+            {
+                closeEnemies.Add(obj);
+            }
+        }
+        return closeEnemies.ToArray();
+    }
+    
+    private void SetSizeText()
+    {
+        sizeText.text = "Current Size: " + playerStats.GetSize().ToString();
     }
 }
