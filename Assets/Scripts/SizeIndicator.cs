abc8c5e33489a5ad23cce792a2914fcf9ae5875a
EthanAmundson0 <eamunds3@colostate.edu>
2025-11-20 11:36:16 -0700

    Added Size indicator to creatures

--- Assets/Scripts/SizeIndicator.cs
+++ Assets/Scripts/SizeIndicator.cs
@@ -1,43 +1,67 @@
+using System;
 using UnityEngine;
 
 public class SizeIndicator : MonoBehaviour
 {
-    public Transform player;
-    public GameObject indicatorPrefab;
+    [SerializeField] GameObject indicatorPrefab;
+    [SerializeField] bool isDungeonEnemy;
     private GameObject indicatorInstance;
     private Renderer indicatorRenderer;
-    public float sizeValue = 5f; // manually set this per enemy
-    public float playerSize = 10f; // manually set or assign from player
-    
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private GameObject playerObject;
+    private Transform player;
+    private SizeStats playerStats;
+    private SizeStats enemyStats;
+
+
+    void OnEnable()
     {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        if (playerObject != null)
+        {
+            player = playerObject.transform;
+            playerStats = playerObject.GetComponent<SizeStats>();
+        }
+
         indicatorInstance = Instantiate(indicatorPrefab, transform);
-        indicatorInstance.transform.localPosition = new Vector3(0, 2f, 0);
+        if (isDungeonEnemy)
+        {
+            indicatorInstance.transform.localPosition = new Vector3(0, 0.05f, 0);
+            indicatorInstance.transform.localScale = Vector3.one * 0.01f;
+        } else
+        {
+            indicatorInstance.transform.localPosition = new Vector3(0, 2f, 0);
+            indicatorInstance.transform.localScale = Vector3.one * 0.8f;            
+        }
+
         
         indicatorRenderer = indicatorInstance.GetComponent<Renderer>();
+
+        enemyStats = GetComponent<SizeStats>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (player == null || indicatorRenderer == null) return;
 
-        // Show/hide the indicator depending on M key
-        bool showIndicator = Input.GetKey(KeyCode.M);
+        // Show/hide the indicator depending on left shift key
+        bool showIndicator = Input.GetKey(KeyCode.LeftShift);
 
         indicatorRenderer.gameObject.SetActive(showIndicator);
 
         // Only change color if indicator is visible
         if (showIndicator)
         {
+            int sizeValue = enemyStats.GetSize();
+            int playerSize = playerStats.GetSize();
             if (sizeValue < playerSize)
                 indicatorRenderer.material.color = Color.green;
             else if (sizeValue > playerSize)
                 indicatorRenderer.material.color = Color.red;
             else
                 indicatorRenderer.material.color = Color.yellow;
+
+            Debug.Log("Showing Indicator!");
         }
+
     }
 }
